cbe53f7f1fb5f497f7ebd9f595b543a7527e5dde
ibrahimaydin <55055268+ibrahimaydindev@users.noreply.github.com>
2024-07-25 16:55:33 +0300

    CoderArena Version 1.1
    - Google services json added.

--- app/src/main/java/com/coder/arena/ui/activity/MainActivity.kt
+++ app/src/main/java/com/coder/arena/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.coder.arena
+package com.coder.arena.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.coder.arena.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
