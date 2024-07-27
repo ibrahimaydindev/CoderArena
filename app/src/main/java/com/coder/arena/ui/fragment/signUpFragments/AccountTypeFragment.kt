6f7b8c25b123a3c83a529ed269fd81441e7cac83
serdarrahmet <ahmetserdarkarabulut1122@gmail.com>
2024-07-28 00:02:51 +0300

    CoderArena Version 1.5
    - Done something.

--- app/src/main/java/com/coder/arena/ui/fragment/signUpFragments/AccountTypeFragment.kt
+++ app/src/main/java/com/coder/arena/ui/fragment/signUpFragments/AccountTypeFragment.kt
@@ -5,21 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 import com.coder.arena.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AccountTypeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AccountTypeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +20,23 @@ class AccountTypeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_type, container, false)
+        val view = inflater.inflate(R.layout.fragment_account_type, container, false)
+
+        val skipButton = view.findViewById<View>(R.id.skip_button)
+        skipButton.setOnClickListener {
+            navigateNextFragment()
+        }
+
+        return view
+    }
+    private fun navigateNextFragment() {
+        val fragment = AccountDetailsFragment()
+        val transaction: FragmentTransaction = parentFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_account_type_container, fragment)
+        transaction.addToBackStack(null) // onBackPressed
+        transaction.commit()
     }
 
 }
