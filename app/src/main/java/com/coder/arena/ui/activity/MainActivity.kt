f569f7026d2161e6b22fb715b59eabc3022284f3
Ufuk Yıldız <ufukyldz@proton.me>
2024-07-27 20:49:38 +0300

    initial commit on mac

--- app/src/main/java/com/coder/arena/ui/activity/MainActivity.kt
+++ app/src/main/java/com/coder/arena/ui/activity/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.coder.arena.R
 
+// Main Activity
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
